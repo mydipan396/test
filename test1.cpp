2c708cc162be7cfa797d40e3dd117c502fe47719
wuqinglang <wuqinglang@corp.netease.com>
2024-08-20 10:56:00 +0800

    #commit

--- test1.cpp
+++ test1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Hello!" << std::endl;
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
